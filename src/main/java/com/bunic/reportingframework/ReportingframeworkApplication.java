e14c4f6ddb609ebe24f36b5f4a98e023d844953c
Pradeep Kumar Vishvakarma <pradeepv4919@gmail.com>
2024-12-21 23:05:49 +0530

    Initial commit

---
+++ src/main/java/com/bunic/reportingframework/ReportingframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.bunic.reportingframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bunic/reportingframework/ReportingframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunic.reportingframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
